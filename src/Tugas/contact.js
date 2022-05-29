d93f4a71642f69c1dfab91616ba555474ea37381
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2022-05-29 20:06:49 +0700

    FIXING CSS

--- src/Tugas/contact.js
+++ src/Tugas/contact.js
@@ -21,19 +21,17 @@ class Contact extends Component {
                     <Card
                         src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720"
                         title="Rajendra Rakha"
-                        description="1941720080 | 3H"
+                        description="1941720080 | 3H | Frotend Engineer"
+                        
                     />
                     
                     <Card
                         src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720"
-                        title="Ariono Septian"
-                        description="1941720080 | 3H"
+                        title="Ariono Septian"     
+                        description="1941720092 | 3H | Backend Engineer"
+
                     />
-                    <Card
-                        src="https://a0.muscache.com/im/pictures/fdb46962-10c1-45fc-a228-d0b055411448.jpg?im_w=720"
-                        title="Meuri Zari "
-                        description="1941720080 | 3H"
-                    />
+                   
                 </div>
 
 
